55fad40ef334d60acddf67184bb4f3e050ed8d79
Sergey Melnikov <jettpy@yandex.ru>
2022-11-16 13:34:31 +0300

    Добавлен попап и адаптивная верстка

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import image from '../../images/image.jpg';
 import Canvas from '../Canvas/Canvas.js';
-import Popup from '../Popup/Popup';
+import Popup from '../Popup/Popup.js';
+import { useState } from 'react';
 
 function App() {
 
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
   function getSelectedData() {
-    console.log('Тест')
+    setIsPopupOpen(true);
+  }
+
+  function closePopUp() {
+    setIsPopupOpen(false);
   }
 
   return (
@@ -15,8 +22,8 @@ function App() {
         <img className='main__image' src={image} alt='Изображение с объектом'></img>
         <Canvas></Canvas>
       </div>
-      <button className='main_add-button button' onClick={getSelectedData}>Добавить</button>
-      <Popup></Popup>
+      <button className='button' type="button"  onClick={getSelectedData}>Добавить</button>
+      <Popup isOpen={isPopupOpen} onClose={closePopUp}></Popup>
     </main>
   );
 }
--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,9 +1,30 @@
 import './Popup.css';
 
-function Popup() {
+function Popup({ isOpen, onClose }) {
 
   return (
-    <div className='popup'></div>
+    <div className={`popup${isOpen ? " popup_opened" : ""}`}>
+      <div className="popup__content">
+        <div className='popup__message'>
+          <p>&#123;</p>
+          <p>&nbsp;"id": "1",</p>
+          <p>&nbsp;"object_id": "1",</p>
+          <p>&nbsp;"objectClass": "car",</p>
+          <p>&nbsp;"region": &#123;</p>
+          <p>&nbsp;&nbsp;"origin": &#123;</p>
+          <p>&nbsp; &nbsp;&nbsp;"x": "1306",</p>
+          <p>&nbsp; &nbsp;&nbsp;"y": "1110"</p>
+          <p>&nbsp;&nbsp;&#125;,</p>
+          <p>&nbsp;&nbsp;"size": &#123;</p>
+          <p>&nbsp; &nbsp;&nbsp;"width": "794",</p>
+          <p>&nbsp; &nbsp;&nbsp;"height": "386"</p>
+          <p>&nbsp;&nbsp;&#125;</p>
+          <p>&nbsp;&#125;</p>
+          <p>&#125;</p>
+        </div>
+        <button className="button" type="button" onClick={onClose}>Закрыть</button>
+      </div>
+    </div>
   );
 }
 
